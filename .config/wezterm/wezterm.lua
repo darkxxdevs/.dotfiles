dd159da85ee5e0244e34d39b4a12e5eb43aac518
arpit <arpit2003y@gmail.com>
2024-05-13 09:45:57 +0530

    chore(term):modified_term_config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,18 +5,15 @@ local config = {}
 config.font = wezterm.font("JetBrains Mono NF semiBold")
 
 -- config.color_scheme = "Batman"
-config.color_scheme = "Tango (terminal.sexy)"
+config.color_scheme = "Sequoia Moonlight"
 
 config.colors = {
 	background = "#0a0b0c",
 	foreground = "#cccccc",
 }
 
-config.harfbuzz_features = {
-	"calt=0",
-}
--- config.window_frame = {
--- 	-- font = wezterm.font({ family = "Liga SFMono Nerd Font", weight = "Medium" }),
+-- config.harfbuzz_features = {
+-- 	"calt=0",
 -- }
 
 config.font_size = 10.6
@@ -25,6 +22,13 @@ config.enable_tab_bar = false
 
 config.window_close_confirmation = "NeverPrompt"
 
-config.default_prog = { "/usr/bin/fish", "-l" }
+config.window_padding = {
+	top = 0,
+	bottom = 0,
+	left = 0,
+	right = 0,
+}
+
+config.default_prog = { "/usr/bin/bash", "-l" }
 
 return config
